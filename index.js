3d2beb550d30f27588a83594a69892c923a42234
IYABA <seokhunv@gmail.com>
2022-04-23 12:14:27 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const express = require('express')
 const app = express()
 const port = 5000
+
+const config = require('./config/key')
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://admin:1234@boilerplate.9awsr.mongodb.net/boilerplate?retryWrites=true&w=majority').then(() => console.log('MongoDB Connected...'))
+mongoose.connect(config.mongoURI).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
+const bodyParser = require('body-parser')
+const {User} = require("./models/User")
+
+//application/x-www-form-urlencorded
+app.use(bodyParser.urlencoded({extended: true}))
+
+//application/json
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
-  res.send('Hello World! 안녕하세요!')
+  res.send('Hello World! 안녕하세요! 반갑습니다.')
+})
+
+app.post('/register', (req, res) => {
+  //회원가입시 Client가 입력한 데이터들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body)
+  user.save((err, userInfo) =>{
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
